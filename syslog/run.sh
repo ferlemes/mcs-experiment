9b4d1a693481b59eadf4eff0f4fd023904cb9155
ferlemes <ferlemes@gmail.com>
2021-03-14 02:11:41 -0300

    Adding timestamp to the request data

--- syslog/run.sh
+++ syslog/run.sh
@@ -33,7 +33,7 @@ handler.setLevel(logging.DEBUG)
 handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 logger.addHandler(handler)
 
-haproxy_log_format = re.compile(r"^.* ([^ ]+) ([^ ]+)/([^ ]+) ([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+) ([0-9]+) ([0-9]+) [^ ]+ [^ ]+ [^ ]+ ([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+) ([0-9]+)/([0-9]+) \"([A-Z]+) ([^ ]+) ([^ ]+)\".*$")
+haproxy_log_format = re.compile(r"^.* \[([^ ]+)\] ([^ ]+) ([^ ]+)/([^ ]+) ([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+) ([0-9]+) ([0-9]+) [^ ]+ [^ ]+ [^ ]+ ([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+) ([0-9]+)/([0-9]+) \"([A-Z]+) ([^ ]+) ([^ ]+)\".*$")
 
 
 if 'DETECTOR_URL' in os.environ:
@@ -48,30 +48,32 @@ class SyslogUDPHandler(socketserver.BaseRequestHandler):
 	def handle(self):
 		data = bytes.decode(self.request[0].strip())
 		socket = self.request[1]
+		logger.info("String= %s", str(data))
 		search = haproxy_log_format.search(str(data))
 		if search:
 			data = search.groups()
 			log_entry = {
-				"frontend":									data[0],
-				"backend":									data[1],
-				"server":									data[2],
-				"time_to_receive_request":					data[3],
-				"time_in_queue":							data[4],
-				"time_to_tcp_connect":						data[5],
-				"time_to_get_response":						data[6],
-				"total_time_active":						data[7],
-				"http_status":								data[8],
-				"bytes_count":								data[9],
-				"concurrent_connections_haproxy":			data[10],
-				"concurrent_connections_frontend":			data[11],
-				"concurrent_connections_backend":			data[12],
-				"concurrent_active_connections_on_server":	data[13],
-				"connection_retry_attempts":				data[14],
-				"queue1":									data[15],
-				"queue2":									data[16],
-				"http_verb":								data[17],
-				"http_path":								data[18],
-				"http_protocol":							data[19]
+				"timestamp":								data[0],
+				"frontend":									data[1],
+				"backend":									data[2],
+				"server":									data[3],
+				"time_to_receive_request":					data[4],
+				"time_in_queue":							data[5],
+				"time_to_tcp_connect":						data[6],
+				"time_to_get_response":						data[7],
+				"total_time_active":						data[8],
+				"http_status":								data[9],
+				"bytes_count":								data[10],
+				"concurrent_connections_haproxy":			data[11],
+				"concurrent_connections_frontend":			data[12],
+				"concurrent_connections_backend":			data[13],
+				"concurrent_active_connections_on_server":	data[14],
+				"connection_retry_attempts":				data[15],
+				"queue1":									data[16],
+				"queue2":									data[17],
+				"http_verb":								data[18],
+				"http_path":								data[19],
+				"http_protocol":							data[20]
 			}
 			try:
 				response = requests.post(detector_url, data = json.dumps(log_entry), headers={'Content-Type': 'application/json'})
